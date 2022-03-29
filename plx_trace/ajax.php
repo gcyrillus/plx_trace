a63e4536e69add2adc1c2ac374bc2ab4cf1c9e09
Grib Cyr <1832331+gcyrillus@users.noreply.github.com>
2022-03-29 09:09:15 +0200

    Add files via upload
    [update]

---
+++ plx_trace/ajax.php
@@ -0,0 +1,23 @@
+<?php
+
+$arr_file_ext = ['gpx'];
+$dir= $_GET['dir'];
+ foreach($_FILES as $filename) {
+	 $ext=pathinfo($filename['name']);
+	 if(!in_array($ext['extension'],$arr_file_ext)) {
+		echo " Fichier avec l'extension gpx requis";
+		return;
+	 }
+}
+if (!file_exists('$dir')) {
+    mkdir('$dir', 0777);
+}
+  
+$filename = $_FILES['file']['name'];
+// if(file_exists('gpx/'.$dir.'/'.$filename)) { $filename=" Un fichier du même nom existe déja.";}
+// else {
+move_uploaded_file($_FILES['file']['tmp_name'], 'gpx/'.$dir.'/'.$filename);
+// }
+echo $filename ;
+die;
+?>
