36cc8487b95536ba13840e8a8a862a878befbae5
Grib Cyr <1832331+gcyrillus@users.noreply.github.com>
2022-04-04 21:29:04 +0200

    Update plx_trace.js

--- ckeditor/ckeditor/plugins/templates/templates/plx_trace.js
+++ ckeditor/ckeditor/plugins/templates/templates/plx_trace.js
@@ -14,14 +14,14 @@ CKEDITOR.addTemplates( 'plx_trace', {
 	templates: [ {
 		title: 'Modéle gpx, pour le plugin plx_trace',
 		image: 'template4.gif',
-		description: 'Un modele pour y inserer le code d\'une carte gpx. Uniquement compatible avec le plugin <b>plx_trace</b>',
-		html: '<h3>Titre: Remplacer ce titre et le texte d\'aide par les votres.</h3>'+
-		'<p><strong><u>Aide:</u></strong>: Pour finaliser l\'insertion de votre trace GPX, <b style="color:red">veuillez, passer en mode </b><i>Source</i> en cliquant l\'icône <img src="../../plugins/ckeditor/ckeditor/plugins/sourcearea/icons/hidpi/source.png"> Source ,<b style="color:red"> puis remplacer et mettre à jour le lien</b> <i><code>plugins/plx_trace/gpx/<b style="color:tomato">test/etape1.gpx</b> </code></i> en indiquant celui de votre fichier gpx.(repertoire et nom de fichier)</p>'+
+		description: 'Un modèle pour y inserer le code d\'une carte gpx. Uniquement compatible avec le plugin <b>plx_trace</b>',
+		html: '<h3>Titre: Remplacez ce titre et le texte d\'aide par les votres.</h3>'+
+		'<p><strong><u>Aide:</u></strong>: Pour finaliser l\'insertion de votre trace GPX, <b style="color:red">veuillez, passez en mode </b><i>Source</i> en cliquant l\'icône <img src="../../plugins/ckeditor/ckeditor/plugins/sourcearea/icons/hidpi/source.png"> Source ,<b style="color:red"> puis remplacer et mettre à jour le lien</b> <i><code>plugins/plx_trace/gpx/<b style="color:tomato">test/etape1.gpx</b> </code></i> en indiquant celui de votre fichier gpx.(repertoire et nom de fichier)</p>'+
 		' '+
 		'<script>const gpxFile="plugins/plx_trace/gpx/demo/demo.gpx";</script>'+
 		'<div id="myMapGpx">'+
 		'  <img src="../../plugins/ckeditor/ckeditor/plugins/templates/templates/images/map.jpg" style="width:80%;margin:1em auto;display:block;">'+
-		'  <p style="text-align:center;text-decoration:underline;">Cette carte et ce texte seront remplacer par votre carte si le plugin <b>plx_trace</b> est actif.</p>'+
+		'  <p style="text-align:center;text-decoration:underline;">Cette image et ce texte seront remplacés par votre carte si le plugin <b>plx_trace</b> est actif.</p>'+
 		'</div>'+
 		'<p data-use="plx_trace">&nbsp;</p>'+
 		' '+
